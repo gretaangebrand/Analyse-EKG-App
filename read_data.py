c6c9a16a815d7e7b030a57822bab19d782593e24
gretaangebrand <ag9716@mci4me.at>
2024-04-24 15:21:08 +0200

    Pfad Bild festlegen

--- read_data.py
+++ read_data.py
@@ -22,26 +22,40 @@ def get_person_list(person_data):
         names.append(eintrag['lastname'] + ', ' + eintrag['firstname'])
     return names
 
+def find_person_data_by_name(suchstring):
+    """ Eine Funktion der Nachname, Vorname als ein String übergeben wird
+    und die die Person als Dictionary zurück gibt"""
+
+    #Namen als Variablen für die Funktionen als neue Liste festlegen
+    person_data = get_person_data()
+    print(names)
+    if suchstring == "None":
+        return {}
+
+    two_names = suchstring.split(", ")
+    vorname = two_names[1]
+    nachname = two_names[0]
+
+    for eintrag in person_data:
+        print(eintrag)
+        if (eintrag["lastname"] == nachname and eintrag["firstname"] == vorname):
+            print()
+
+            return eintrag
+    else:
+        return {}
+  
 #Testen der Funktionen
 if __name__ == "__main__":
     #Variablen für die Funktionen
     person_data = get_person_data()
     print(person_data)
     names = get_person_list(person_data)
-
-    print(names)
-
-#Name und Bild verbinden
-suchstring  = "Huber, Julian"
-
-# Teilt einen String in und speichert die Ergebnisse in einer Liste
-two_names = suchstring.split(", ")
-vorname = two_names[1]
-nachname = two_names[0]
-
-person_data = get_person_data()
-
-# Nun können wir vergleichen bis wir einen Treffer finden
-for eintrag in person_data:
-    if (eintrag["lastname"] == nachname and eintrag["firstname"] == vorname):
-        print(eintrag)
+    
+    #Name und Bild verbinden
+    suchstring  = "Huber, Julian"
+    name = find_person_data_by_name(suchstring)
+    print(name)
+    
+    #Pfad des Bildes festlegen
+    current_picture_path = name["picture_path"]
